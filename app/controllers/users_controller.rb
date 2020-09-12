745e4509a269a2e845bd3e282c6c6507184c5ab4
ibtechraj <raj_shk@rediffmail.com>
2020-09-12 17:35:00 +0530

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+skip_before_action :require_login, only: [:create]
+
+  def create
+    user = User.create(user_params) 
+    if user.valid?
+        payload = {user_id: user.id}
+        token = encode_token(payload)
+        render json: {user: user, jwt: token}
+    else
+        render json: {errors: user.errors.full_messages}, status: :not_acceptable
+    end
+  end
+
+  private 
+
+  def user_params
+    #params.permit(:email, :password)
+    params.permit(:username, :email, :password, :confirm_password)
+  end
+end
